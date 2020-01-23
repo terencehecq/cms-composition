0e48aa42a41012a8fd644368078ed589e7783ba7
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-23 09:41:47 +0100

    header and footer

--- footer.php
+++ footer.php
@@ -1,3 +1,53 @@
+<?php
+// Template name : Footer
+    // wp_footer();
+?>
+
+<div class="container">
+  <div class="row">
+
+    <footer class="col-12">
+      <div class="footer_top d-flex col-10 mx-auto py-5">
+        <div class="footer_article_link col-8 d-flex align-items-center">
+          <div class="image_rounded mr-3">
+            <img src="<?php bloginfo('template_url'); ?>/img/1500x1250_02.jpg">
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, perspiciatis architecto.</p>
+            <a href="#">Tissus & habillage maison-></a>
+          </div>
+          
+        </div>
+        <div class="footer_horaires col-4 py-4 px-5 d-flex flex-column">
+          <p><b>Horaires</b></p>
+          <p>Lundi > Dimanche : (Fermé le mardi) 10h > 18h30</p>
+        </div>
+      </div>
+      <div class="footer_details d-flex col-10 mx-auto mt-5 justify-content-between">
+        <div>
+          <h3>Rendez-nous visite !</h3>
+          <a href="#">Google maps -></a>
+        </div>
+        <div>
+          <p>Route de Hamoir, 87 - 4500 Huy</p>
+          <p>info@meublescomposition.be</p>
+          <p>+32 (0)85 32 12 56</p>
+        </div>
+        <div class="footer_social">
+          <a href="#">F</a>
+          <a href="#">I</a>
+          <a href="#">P</a>
+			<p>© Composition 2020 - BeCode</p>
+		</div>
+	</div>
+  
+</footer>
+</div>
+</div>
+
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,14 +1,45 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Composition</title>
-</head>
-<body>
+<?php
+// Template name : header
 
-<p>Ceci est le header</p>
-<!-- Insérer navigation ici -->
+?><!doctype html>
+<html <?php language_attributes(); ?> class="m-0">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<?php wp_head(); ?>
+</head>
+
+<body>
+<div class="container nav-container">
+        <nav class="navbar navbar-expand-lg px-5 d-flex navbar-dark navbar-custom">
+            <a class="navbar-brand" href="#">Composition</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/wp-challenge">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Magasin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/wp-challenge/archi">Architecte d'intérieur</a>
+				</li>
+				<li class="nav-item">
+                    <a class="nav-link" href="/wp-challenge/about">A propos</a>
+				</li>
+				<li class="nav-item">
+                    <a class="nav-link" href="/wp-challenge/blog">Blog</a>
+				</li>
+				<li class="nav-item">
+                    <a class="nav-link" href="/wp-challenge/contact">Contact</a>
+				</li>
+            </div>
+        </nav>
+</div>
