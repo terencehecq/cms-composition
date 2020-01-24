106afd77c4b262e2a2b3595a49d4d5198dda6f91
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-24 14:55:00 +0100

    udate footer with terence version

--- footer.php
+++ footer.php
@@ -5,42 +5,48 @@
 
 <div class="container-fluid">
   <div class="row">
+<footer class="col-12">
+	<div class="footer_top d-none d-md-flex col-10 mx-auto py-5">
+		<div class="footer_article_link col-8 d-flex align-items-center">
+			<div class="image_rounded mr-3">
+				<img src="<?php bloginfo('template_url'); ?>/img/1500x1250_02.jpg">
+			</div>
+			<div>
+				<p>Lorem ipsum dolor sit amet coucou petite perruche.</p>
+				<a href="#">Tissus & habillage</a>
+			</div>
 
-    <footer class="col-12">
-      <div class="footer_top d-flex col-10 mx-auto py-5">
-        <div class="footer_article_link col-8 d-flex align-items-center">
-          <div class="image_rounded mr-3">
-            <img src="<?php bloginfo('template_url'); ?>/img/1500x1250_02.jpg">
-          </div>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, perspiciatis architecto.</p>
-            <a href="#">Tissus & habillage maison-></a>
-          </div>
-          
-        </div>
-        <div class="footer_horaires col-4 py-4 px-5 d-flex flex-column">
-          <p><b>Horaires</b></p>
-          <p>Lundi > Dimanche : (Fermé le mardi) 10h > 18h30</p>
-        </div>
-      </div>
-      <div class="footer_details d-flex col-10 mx-auto mt-5 justify-content-between">
-        <div>
-          <h3>Rendez-nous visite !</h3>
-          <a href="#">Google maps -></a>
-        </div>
-        <div>
-          <p>Route de Hamoir, 87 - 4500 Huy</p>
-          <p>info@meublescomposition.be</p>
-          <p>+32 (0)85 32 12 56</p>
-        </div>
-        <div class="footer_social">
-          <a href="#">F</a>
-          <a href="#">I</a>
-          <a href="#">P</a>
+		</div>
+		<div class="footer_horaires col-4 py-4 px-5 d-flex flex-column">
+			
+			<p><img src="<?php bloginfo('template_url'); ?>/img/pictos/time-clock.svg"><b>Horaires</b></p>
+			<p>Lundi > Dimanche : (Fermé le mardi) 10h > 18h30</p>
+		</div>
+	</div>
+	<div class="footer_details d-md-flex col-md-10 mx-auto pt-5 justify-content-between">
+		<div class="col-12 col-md-4 mb-5">
+			<h3>Rendez-nous visite !</h3>
+			<a href="#" class="maps">Google maps</a>
+		</div>
+		<div class="col-md-4 footer_address">
+			<p><img src="<?php bloginfo('template_url'); ?>/img/pictos/marker-road.svg"> Route de Hamoir, 87 - 4500 Huy</p>
+			<p><img src="<?php bloginfo('template_url'); ?>/img/pictos/icon-arrobas.svg"> info@meublescomposition.be</p>
+			<p><img src="<?php bloginfo('template_url'); ?>/img/pictos/phone-2.svg"> +32 (0)85 32 12 56</p>
+		</div>
+		<div class="footer_horaires col-12 py-4 px-5 mt-4 d-flex flex-column d-md-none">
+			<p><b>Horaires</b></p>
+			<p>Lundi > Dimanche : (Fermé le mardi) 10h > 18h30</p>
+		</div>
+		<div class="footer_social col-md-4 text-center mt-4 text-md-right">
+			<div class="social_links mb-3">
+				<a href="#"><img src="<?php bloginfo('template_url'); ?>/img/pictos/logo-facebook.svg"></a>
+				<a href="#"><img src="<?php bloginfo('template_url'); ?>/img/pictos/logo-instagram.svg"></a>
+				<a href="#"><img src="<?php bloginfo('template_url'); ?>/img/pictos/logo-pinterest.svg"></a>
+			</div>
 			<p>© Composition 2020 - BeCode</p>
 		</div>
 	</div>
-  
+
 </footer>
 </div>
 </div>
